496aa7cdad79671810dbf80b29ab6b05a2c7229c
Lamiaa78 <100521392+Lamiaa78@users.noreply.github.com>
2024-12-12 13:53:36 +0100

    Optimisation

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,27 @@
 #!/bin/sh
 #  chmod +x deploy.sh 
-docker build -t front-end .
+
+# Charger les variables d'environnement depuis le fichier .env
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
+# Définir le Dockerfile en fonction de l'environnement
+DOCKERFILE="Dockerfile.prod"
+if [ "$APP_ENV" = "development" ]; then
+    DOCKERFILE="Dockerfile.dev"
+fi
+
+# Supprimer les conteneurs existants
+if [ "$(docker ps -aq -f name=front-end)" ]; then
+    docker rm -f front-end
+fi
+
+# Construire l'image Docker avec le cache
+docker build -t front-end -f $DOCKERFILE .
+
+# Exécuter le conteneur Docker
 docker run -d --name front-end -p 8080:8080 --env APP_ENV=${APP_ENV} front-end
+
+# Afficher les logs du conteneur
 docker logs -f front-end
--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-npm i
-if [ "$APP_ENV" = 'development' ]; then 
-  npm run serve
-else 
-  npm install -g http-server
-  npm run build
-  http-server dist
-fi
